11aeaeeaa7f52a53a2880dab84d2c0c4feb1f945
dmitri <deemok@gmail.com>
2021-01-20 13:02:51 +0100

    Address review comments

--- lib/client/keyagent_test.go
+++ lib/client/keyagent_test.go
@@ -451,9 +451,7 @@ func (s *KeyAgentTestSuite) makeKey(username string, allowedLogins []string, ttl
 }
 
 func startDebugAgent() (closer func(), err error) {
-	startedC := make(chan struct{})
 	rand.Seed(time.Now().Unix())
-
 	socketpath := filepath.Join(os.TempDir(),
 		fmt.Sprintf("teleport-%d-%d.socket", os.Getpid(), rand.Uint32()))
 
@@ -465,6 +463,7 @@ func startDebugAgent() (closer func(), err error) {
 	systemAgent := agent.NewKeyring()
 	os.Setenv(teleport.SSHAuthSock, socketpath)
 
+	startedC := make(chan struct{})
 	doneC := make(chan struct{})
 	var wg sync.WaitGroup
 	wg.Add(2)
